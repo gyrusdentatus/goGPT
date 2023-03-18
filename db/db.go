36126b6a9900e43e1a73f2b874b63564f7804662
Hans Bricks <hans@dialout.net>
2023-03-18 19:03:09 +0000

    db mod

---
+++ db/db.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func initDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./conversation_history.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, role TEXT, content TEXT);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("%q: %s\n", err, sqlStmt)
+	}
+
+	return db
+}
+
+func addMessage(db *sql.DB, role, content string) {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO messages(role, content) VALUES(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(role, content)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx.Commit()
+}
+
+func getConversationHistory(db *sql.DB) []openai.ChatCompletionMessage {
+	rows, err := db.Query("SELECT role, content FROM messages")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var messages []openai.ChatCompletionMessage
+	for rows.Next() {
+		var role, content string
+		err = rows.Scan(&role, &content)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    role,
+			Content: content,
+		})
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return messages
+}
+
--- main.go
+++ main.go
@@ -1,68 +1,78 @@
 package main
 
 import (
-    "bufio"
-    "context"
-    "fmt"
-    "os"
-    "strings"
-    openai "github.com/sashabaranov/go-openai"
-    "github.com/fatih/color"
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/fatih/color"
+	"GoGPT/db"
 )
 
 var (
-    promptStyle       = color.New(color.FgHiMagenta, color.Bold)
-    userInputStyle    = color.New(color.FgHiCyan)
-    goGPTResponseStyle = color.New(color.FgHiGreen)
-    errorStyle        = color.New(color.FgHiRed, color.Bold)
+	promptStyle       = color.New(color.FgHiMagenta, color.Bold)
+	userInputStyle    = color.New(color.FgHiCyan)
+	goGPTResponseStyle = color.New(color.FgHiGreen)
+	errorStyle        = color.New(color.FgHiRed, color.Bold)
 )
 
-func PrintStyled(c *color.Color, format string, a ...interface{}) {
-    c.PrintfFunc()(format, a...)
+func printStyled(c *color.Color, format string, a ...interface{}) {
+	c.PrintfFunc()(format, a...)
 }
 
 func main() {
-    client := openai.NewClient("YOUR TOKEN")
+	client := openai.NewClient("your token")
 
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-    PrintStyled(promptStyle, "Welcome to GoGPT! Please choose a role: writer, programmer, or teacher\n")
-    role, _ := reader.ReadString('\n')
-    role = strings.TrimSpace(strings.ToLower(role))
+	printStyled(promptStyle, "Welcome to GoGPT! Please choose a role: writer, programmer, or teacher\n")
+	role, _ := reader.ReadString('\n')
+	role = strings.TrimSpace(strings.ToLower(role))
 
-    for {
-        PrintStyled(promptStyle, "Enter your message (type 'exit' to quit):\n")
-        userInput, _ := reader.ReadString('\n')
-        userInput = strings.TrimSpace(userInput)
+	// Initialize the database
+	db := initDB()
+	defer db.Close()
 
-        if userInput == "exit" {
-            break
-        }
+	for {
+		printStyled(promptStyle, "Enter your message (type 'exit' to quit):\n")
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.TrimSpace(userInput)
 
-        resp, err := client.CreateChatCompletion(
-            context.Background(),
-            openai.ChatCompletionRequest{
-                Model: openai.GPT3Dot5Turbo,
-                Messages: []openai.ChatCompletionMessage{
-                    {
-                        Role:    openai.ChatMessageRoleSystem,
-                        Content: fmt.Sprintf("You are a %s now. I am here to assist you with any questions you have.", role),
-                    },
-                    {
-                        Role:    openai.ChatMessageRoleUser,
-                        Content: userInput,
-                    },
-                },
-            },
-        )
+		if userInput == "exit" {
+			break
+		}
 
-        if err != nil {
-            PrintStyled(errorStyle, "ChatCompletion error: %v\n", err)
-            return
-        }
+		// Fetch the conversation history
+		messages := getConversationHistory(db)
 
-        PrintStyled(userInputStyle, "You: %s\n", userInput)
-        PrintStyled(goGPTResponseStyle, "GoGPT (%s): %s\n", role, resp.Choices[0].Message.Content)
-    }
+		// Add user message to the history
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: userInput,
+		})
+
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model:    openai.GPT3Dot5Turbo,
+				Messages: messages,
+			},
+		)
+
+		if err != nil {
+			printStyled(errorStyle, "ChatCompletion error: %v\n", err)
+			return
+		}
+
+		printStyled(userInputStyle, "You: %s\n", userInput)
+		printStyled(goGPTResponseStyle, "GoGPT (%s): %s\n", role, resp.Choices[0].Message.Content)
+
+		// Add messages to the database
+		addMessage(db, openai.ChatMessageRoleUser, userInput)
+		addMessage(db, openai.ChatMessageRoleAssistant, resp.Choices[0].Message.Content)
+	}
 }
 
